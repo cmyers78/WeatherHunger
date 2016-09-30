bf76f2cde32bff4c1e5e5fc1a778ad34e7641aa2
Chris Myers <cmyers78@gmail.com>
2016-09-30 14:38:39 -0400

    slow going on writing to a directory...

--- WeatherHunger/WeatherHunger/DocumentsCSV.swift
+++ WeatherHunger/WeatherHunger/DocumentsCSV.swift
@@ -17,18 +17,41 @@ class DocumentsCSV: NSObject {
         
     }
     
+    //initialize with weather info, and restaurant names
+    // self.newline = "weatherinfo param, rest1 param, rest2 param, rest3 param
+    
     
     // Create an api that writes a file which summarizes the data gathered in csv format
-    func writeToDocsDirectory() {
+    
+    func writeToFile(content: String, fileName: String) {
         
+        let contentToAppend = content+"\n"
+        //NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+        let filePath = NSHomeDirectory() + "/Documents/" + fileName
+        
+        //Check if file exists
+        if let fileHandle = NSFileHandle(forWritingAtPath: filePath) {
+            //Append to file
+            fileHandle.seekToEndOfFile()
+            fileHandle.writeData(contentToAppend.dataUsingEncoding(NSUTF8StringEncoding)!)
+        }
+        else {
+            //Create new file
+            do {
+                try contentToAppend.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
+            } catch {
+                print("Error creating \(filePath)")
+            }
+        }
+    }
+
         // take the first cell weather info (current weather)
         // take the three restaurants gathered
         // create a file and write as weatherSummary, rest1, rest2, rest3
         // move on to next line and repeat as location changes.
         // append file to docs directory
         
-        
-    }
+
 
 
 }
